1f447b6b93e23b72a5cadaefa73a61e094b83547
335156154 <335156154@C-2UA4130H27.tdsb.on.ca>
2024-04-18 14:57:51 -0400

    Commit

---
+++ src/main/java/com/mycompany/u2a1_ma/Vehicle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.u2a1_ma;
+
+/**
+ *
+ * @author 335156154
+ */
+public class Vehicle {
+    
+    String liscencePlate;
+    int passengerNum;
+    double passengerFare, fuelEfficiency;
+    
+    
+}
